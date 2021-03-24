e2168498dd5411fcc6360baf74aa4970a2c8c8a0
Yaroslavom <yarykomm@gmail.com>
2021-03-24 13:51:47 +0200

    init slider

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//find elements btn:
+var showPrevBtn = document.getElementById("show-prev");
+var showNextBtn = document.getElementById("show-next");
+var slideImage = document.getElementById("slide-img")
+
+//add events:
+showPrevBtn.addEventListener('click', onShowPrevBtnClick);
+showNextBtn.addEventListener('click', onShowNextBtnClick);
+
+//images array here:
+var imagesStack = ["img/2000000053.jpeg", "img/2000000054.jpeg", "img/2000000055.jpeg", "img/2000000056.jpeg"]
+
+var currentImageIndex = 0;
+slideImage.src = imagesStack[currentImageIndex];
+showPrevBtn.hidden = true;
+
+//function definitions:
+function onShowPrevBtnClick() {
+    currentImageIndex--;
+    slideImage.src = imagesStack[currentImageIndex];
+    showNextBtn.hidden = false;
+
+    //disable prev button
+    if (currentImageIndex === 0 ) {
+        showPrevBtn.hidden = true;
+    }
+}
+function onShowNextBtnClick() {
+    currentImageIndex++;
+    slideImage.src = imagesStack[currentImageIndex];
+    showPrevBtn.hidden = false;
+
+    //disable next button
+    if (currentImageIndex === (imagesStack.length - 1) ) {
+        showNextBtn.hidden = true;
+    }
+}
