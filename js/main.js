8e6055a0e7d7c139127af78a420773ea11b08a87
Yaroslavom <yarykomm@gmail.com>
2021-03-28 20:03:23 +0300

    created constructor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var slider1 = sliderFatory.createNewSlider();
-var slider2 = sliderFatory.createNewSlider();
+var slider1 = new Slider();
+var slider2 = new Slider();
 
 slider1.start("slider1");
 slider2.start("slider2");
--- js/slider.js
+++ js/slider.js
@@ -1,64 +1,48 @@
-var sliderFatory = {
-  createNewSlider: function () {
-    var newSlider = {
-      //images array:
-      imagesStack: [
-        "img/2000000053.jpeg",
-        "img/2000000054.jpeg",
-        "img/2000000055.jpeg",
-        "img/2000000056.jpeg",
-      ],
-      currentImageIndex: 0,
-
-      showPrevBtn: null,
-      showNextBtn: null,
-      slideImage: null,
-
-      start: function (elId) {
-        var that = this;
-
-        var elSelector = "#" + elId;
-        var el = document.querySelector(elSelector);
-        //find elements btn:
-        this.showPrevBtn = el.querySelector(".button-prev");
-        this.showNextBtn = el.querySelector(".button-next");
-        this.slideImage = el.querySelector(".slide-img");
-
-        //add events:
-        this.showPrevBtn.addEventListener("click", function (e) {
-          that.onShowPrevBtnClick(e);
-        });
-        this.showNextBtn.addEventListener("click", function (e) {
-          that.onShowNextBtnClick(e);
-        });
-
-        this.slideImage.src = this.imagesStack[this.currentImageIndex];
-        this.showPrevBtn.hidden = true;
-      },
-
-      onShowPrevBtnClick: function (event) {
-        this.currentImageIndex--;
-        this.slideImage.src = this.imagesStack[this.currentImageIndex];
-        this.showNextBtn.hidden = false;
-
-        //disable prev button
-        if (this.currentImageIndex === 0) {
-          this.showPrevBtn.hidden = true;
-        }
-      },
-
-      onShowNextBtnClick: function (event) {
-        this.currentImageIndex++;
-        this.slideImage.src = this.imagesStack[this.currentImageIndex];
-        this.showPrevBtn.hidden = false;
-
-        //disable next button
-        if (this.currentImageIndex === this.imagesStack.length - 1) {
-          this.showNextBtn.hidden = true;
-        }
-      },
-    };
-
-    return newSlider;
-  },
-};
+function Slider() {
+  this.imagesStack = [
+    "img/2000000053.jpeg",
+    "img/2000000054.jpeg",
+    "img/2000000055.jpeg",
+    "img/2000000056.jpeg",
+  ];
+  this.currentImageIndex = 0;
+  this.showPrevBtn = null;
+  this.showNextBtn = null;
+  this.slideImage = null;
+  this.start = function (elId) {
+    var that = this;
+    var elSelector = "#" + elId;
+    var el = document.querySelector(elSelector);
+    //find elements btn:
+    this.showPrevBtn = el.querySelector(".button-prev");
+    this.showNextBtn = el.querySelector(".button-next");
+    this.slideImage = el.querySelector(".slide-img");
+    //add events:
+    this.showPrevBtn.addEventListener("click", function (e) {
+      that.onShowPrevBtnClick(e);
+    });
+    this.showNextBtn.addEventListener("click", function (e) {
+      that.onShowNextBtnClick(e);
+    });
+    this.slideImage.src = this.imagesStack[this.currentImageIndex];
+    this.showPrevBtn.hidden = true;
+  };
+  this.onShowPrevBtnClick = function (event) {
+    this.currentImageIndex--;
+    this.slideImage.src = this.imagesStack[this.currentImageIndex];
+    this.showNextBtn.hidden = false;
+    //disable prev button
+    if (this.currentImageIndex === 0) {
+      this.showPrevBtn.hidden = true;
+    }
+  };
+  this.onShowNextBtnClick = function (event) {
+    this.currentImageIndex++;
+    this.slideImage.src = this.imagesStack[this.currentImageIndex];
+    this.showPrevBtn.hidden = false;
+    //disable next button
+    if (this.currentImageIndex === this.imagesStack.length - 1) {
+      this.showNextBtn.hidden = true;
+    }
+  };
+}
